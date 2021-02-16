e537d292672f61efc13ce5f6d3672fd62d806ed9
shjman <shjman1@gmail.com>
2021-02-16 22:59:18 +0300

    add navigation to cameraFragment

--- app/src/main/java/com/shjman/polygon/ui/home/refueling/RefuelingFragment.kt
+++ app/src/main/java/com/shjman/polygon/ui/home/refueling/RefuelingFragment.kt
@@ -3,9 +3,16 @@ package com.shjman.polygon.ui.home.refueling
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.shjman.polygon.R
 import com.shjman.polygon.databinding.FragmentRefuelingBinding
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import reactivecircus.flowbinding.android.view.clicks
+import timber.log.Timber
 
 class RefuelingFragment : Fragment(R.layout.fragment_refueling) {
 
@@ -13,5 +20,9 @@ class RefuelingFragment : Fragment(R.layout.fragment_refueling) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.takeCheckPhoto.clicks()
+            .onEach { findNavController().navigate(RefuelingFragmentDirections.fromRefuelingToCamera()) }
+            .catch { Timber.e(it, "binding.takeCheckPhoto.clicks() error") }
+            .launchIn(viewLifecycleOwner.lifecycleScope)
     }
 }
