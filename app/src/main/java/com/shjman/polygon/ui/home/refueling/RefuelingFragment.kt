16a53452997598787285014844b7e17b58bff2de
shjman <shjman1@gmail.com>
2021-02-16 22:42:22 +0300

    update refueiling xml and move strings resources

--- app/src/main/java/com/shjman/polygon/ui/home/refueling/RefuelingFragment.kt
+++ app/src/main/java/com/shjman/polygon/ui/home/refueling/RefuelingFragment.kt
@@ -1,5 +1,7 @@
 package com.shjman.polygon.ui.home.refueling
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.shjman.polygon.R
@@ -8,4 +10,8 @@ import com.shjman.polygon.databinding.FragmentRefuelingBinding
 class RefuelingFragment : Fragment(R.layout.fragment_refueling) {
 
     private val binding by viewBinding(FragmentRefuelingBinding::bind)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
